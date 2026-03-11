3831aa203881bf93c0a073f00909b5c262df2af6
Joalor64 <rhgarcon@gmail.com>
2026-03-11 10:15:07 -0400

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -30,16 +30,39 @@ class OptionsState extends MusicBeatState
 	var selectorLeft:Alphabet;
 	var selectorRight:Alphabet;
 
+	var camFollow:FlxObject;
+	var camFollowPos:FlxObject;
+	var camMain:FlxCamera;
+	var camSub:FlxCamera;
+
 	override function create()
 	{
 		#if DISCORD_ALLOWED
 		DiscordClient.changePresence("Options Menu", null);
 		#end
 
+		camMain = new FlxCamera();
+		camSub = new FlxCamera();
+		camSub.bgColor.alpha = 0;
+
+		FlxG.cameras.reset(camMain);
+		FlxG.cameras.add(camSub, false);
+
+		FlxG.cameras.setDefaultDrawTarget(camMain, true);
+		CustomFadeTransition.nextCamera = camSub;
+
+		camFollow = new FlxObject(0, 0, 1, 1);
+		camFollowPos = new FlxObject(0, 0, 1, 1);
+		add(camFollow);
+		add(camFollowPos);
+		FlxG.camera.follow(camFollowPos, null, 1);
+
+		var yScroll:Float = Math.max(0.25 - (0.05 * (options.length - 4)), 0.1);
 		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
 		bg.color = 0xFFea71fd;
 		bg.updateHitbox();
 		bg.screenCenter();
+		bg.scrollFactor.set(0, yScroll / 3);
 		bg.antialiasing = ClientPrefs.data.globalAntialiasing;
 		add(bg);
 
@@ -51,6 +74,7 @@ class OptionsState extends MusicBeatState
 			var optionText:Alphabet = new Alphabet(0, 0, options[i], true);
 			optionText.screenCenter();
 			optionText.y += (100 * (i - (options.length / 2))) + 50;
+			optionText.scrollFactor.set(0, Math.max(0.25 - (0.05 * (options.length - 4)), 0.1));
 			grpOptions.add(optionText);
 		}
 
@@ -65,15 +89,31 @@ class OptionsState extends MusicBeatState
 		super.create();
 	}
 
+	override function openSubState(subState:FlxSubState)
+	{
+		super.openSubState(subState);
+		if (!(subState is CustomFadeTransition))
+			persistentDraw = persistentUpdate = false;
+	}
+
 	override function closeSubState()
 	{
 		super.closeSubState();
 		ClientPrefs.saveSettings();
+		persistentDraw = persistentUpdate = true;
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		var lerpVal:Float = CoolUtil.boundTo(elapsed * 7.5, 0, 1);
+		camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
+	
+		var mult:Float = FlxMath.lerp(1.07, bg.scale.x, CoolUtil.boundTo(1 - (elapsed * 9), 0, 1));
+		bg.scale.set(mult, mult);
+		bg.updateHitbox();
+		bg.offset.set();
 
 		if (controls.UI_UP_P)
 		{
@@ -106,18 +146,11 @@ class OptionsState extends MusicBeatState
 
 	function changeSelection(change:Int = 0)
 	{
-		curSelected += change;
-		if (curSelected < 0)
-			curSelected = options.length - 1;
-		if (curSelected >= options.length)
-			curSelected = 0;
+		curSelected = FlxMath.wrap(curSelected + change, 0, options.length - 1);
 
-		var bullShit:Int = 0;
-
-		for (item in grpOptions.members)
+		for (num => item in grpOptions.members)
 		{
-			item.targetY = bullShit - curSelected;
-			bullShit++;
+			item.targetY = num - curSelected;
 
 			item.alpha = 0.6;
 			if (item.targetY == 0)
@@ -127,6 +160,8 @@ class OptionsState extends MusicBeatState
 				selectorLeft.y = item.y;
 				selectorRight.x = item.x + item.width + 15;
 				selectorRight.y = item.y;
+				final add:Float = (grpOptions.members.length > 4 ? grpOptions.members.length * 8 : 0);
+				camFollow.setPosition(item.getGraphicMidpoint().x, item.getGraphicMidpoint().y - add);
 			}
 		}
 		FlxG.sound.play(Paths.sound('scrollMenu'));
